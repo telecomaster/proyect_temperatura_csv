b9af46e3ffda1feb1e039e3d4b337dfd8ecd6088
telecomaster <rene.sosa.santos@gmail.com>
2023-06-01 14:05:51 +0200

    Update temperatura mas baja
    se adicionó la ultima ruta del boton

--- app.py
+++ app.py
@@ -29,5 +29,46 @@ def consulta():
     
     return render_template('consulta.html', ciudad=ciudad, resultados=resultados)
 
+@app.route('/consulta_probabilidad', methods=['GET'])
+def consulta_probabilidad():
+    probabilidad = float(request.args.get('probabilidad'))
+    registros = []
+
+    with open('datos.csv', 'r') as archivo:
+        lector_csv = csv.reader(archivo, delimiter=';')
+        next(lector_csv)  # Omitir la primera línea de encabezados
+        for fila in lector_csv:
+            ciudad = fila[0]
+            temperatura = float(fila[1])
+            probabilidad_lluvia = float(fila[2])
+            fecha = fila[3]
+            registros.append({'Ciudad': ciudad, 'Temperatura': temperatura, 'Probabilidad de lluvia': probabilidad_lluvia, 'Fecha': fecha})
+
+    registros_filtrados = [registro for registro in registros if registro['Probabilidad de lluvia'] >= probabilidad]
+
+    ciudad_max_probabilidad = next((registro['Ciudad'] for registro in registros if registro['Probabilidad de lluvia'] == 100), None)
+
+    return render_template('consulta_probabilidad.html', probabilidad=probabilidad, registros=registros_filtrados, ciudad_max_probabilidad=ciudad_max_probabilidad)
+    
+@app.route('/consulta_temperatura_baja', methods=['GET'])
+def consulta_temperatura_baja():
+    registros = []
+
+    with open('datos.csv', 'r') as archivo:
+        lector_csv = csv.reader(archivo, delimiter=';')
+        next(lector_csv)  # Omitir la primera línea de encabezados
+        for fila in lector_csv:
+            ciudad = fila[0]
+            temperatura = float(fila[1])
+            probabilidad_lluvia = float(fila[2])
+            fecha = fila[3]
+            registros.append({'Ciudad': ciudad, 'Temperatura': temperatura, 'Probabilidad de lluvia': probabilidad_lluvia, 'Fecha': fecha})
+
+    temperatura_minima = min(registros, key=lambda x: x['Temperatura'])
+    ciudad_temperatura_minima = temperatura_minima['Ciudad']
+    temperatura_minima_valor = temperatura_minima['Temperatura']
+    fecha_temperatura_minima = temperatura_minima['Fecha']
+
+    return render_template('consulta_temperatura_baja.html', ciudad_temperatura_minima=ciudad_temperatura_minima, temperatura_minima=temperatura_minima_valor, fecha_temperatura_minima=fecha_temperatura_minima)
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
