34222e53751b2e0c066c3dee8a5774b502cf2a7d
telecomaster <rene.sosa.santos@gmail.com>
2023-06-01 12:35:28 +0200

    add messages
    adicioné los mensajes para el primer caso

--- app.py
+++ app.py
@@ -14,15 +14,20 @@ def consulta():
     ciudad = request.args.get('ciudad')
     resultados = []
     with open('datos.csv', 'r') as archivo:
-        lector_csv = csv.reader(archivo)
+        lector_csv = csv.reader(archivo, delimiter=';')  # Utilizar punto y coma como separador
+        next(lector_csv)  # Omitir la primera línea de encabezados
         for fila in lector_csv:
-            print(fila)  # Agregar esta línea para imprimir los datos en la consola del servidor
+            #print(fila)  # Agregar esta línea para imprimir los datos en la consola del servidor
             if fila[0].lower() == ciudad.lower():
                 fecha = fila[3]
                 temperatura = float(fila[1])
                 probabilidad_lluvia = float(fila[2])
                 resultados.append((fecha, temperatura, probabilidad_lluvia))
     
+    # Realiza la conversión a float antes de pasar los datos a la plantilla
+    resultados = [(fecha, float(temperatura), float(probabilidad_lluvia)) for fecha, temperatura, probabilidad_lluvia in resultados]
+    
     return render_template('consulta.html', ciudad=ciudad, resultados=resultados)
+
 if __name__ == '__main__':
-   app.run(host= "0.0.0.0", port=5000)
+    app.run(host='0.0.0.0', port=5000)
